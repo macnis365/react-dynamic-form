6e1d292af82e699367bcfcb92f816e8c99da98e4
macnis365 <macnis365@gmail.com>
2019-03-16 20:02:42 +0530

    added rule json

--- src/App.js
+++ src/App.js
@@ -5,12 +5,7 @@ import './App.css';
 class App extends Component {
   state = {
     data: [
-      {id: 1, name:"a", age:29, qualification:"B.Com",rating:3,gender:"male",
-          city:"Kerala",skills:["reactjs","angular","vuejs"]},
-      {id: 2, name:"b", age:35, qualification:"B.Sc",rating:5,gender:"female",
-          city:"Mumbai",skills:["reactjs","angular"]},
-      {id: 3, name:"c", age:42, qualification:"B.E",rating:3,gender:"female",
-        city:"Bangalore",skills:["reactjs"]},
+      
     ],
     current: {}
   }
@@ -62,27 +57,59 @@ class App extends Component {
         <DynamicForm className="form"
           title = "Registration"
           defaultValues = {this.state.current}
-          model={[
-            {key: "name", label: "Name", props: {required: true}},
-            {key: "age",label: "Age", type: "number"},
-            {key: "rating",label: "Rating", type: "number", props:{min:0,max:5}},
-            {key: "gender",label: "Gender", type:"radio",options:[
-              {key:"male",label:"Male",name:"gender",value:"male"},
-              {key:"female",label:"Female",name: "gender",value:"female"}
-            ]},
-            {key: "qualification",label: "Qualification"},
-            {key: "city",label:"City", type:"select", value: "Kerala", options: [
-                {key:"mumbai",label:"Mumbai",value:"Mumbai"},
-                {key:"bangalore",label:"Bangalore",value:"Bangalore"},
-                {key:"kerala",label:"Kerala",value:"Kerala"},
-            ]},
-            {key: "skills",label:"Skills", type:"checkbox", options: [
-                {key:"reactjs",label:"ReactJS",value:"reactjs"},
-                {key:"angular",label:"Angular",value:"angular"},
-                {key:"vuejs",label:"VueJS",value:"vuejs"},
-            ]},
-
-          ]}
+          model={
+            [
+               {
+                  key:"country",
+                  label:"Country",
+                  value:"Dubai"
+               },
+               {
+                  key:"name",
+                  label:"Name",
+                  value:"VAT Dubai"
+               },
+               {
+                  key:"feeInterest",
+                  label:"VAT Percentage",
+                  value:5
+               },
+               {
+                  key:"dealType",
+                  label:"Deal Type",
+                  type:"select",
+                  options:[
+                     {
+                        key:"B",
+                        label:"Bilateral",
+                        value:"B"
+                     },
+                     {
+                        key:"S",
+                        label:"Syndicate Agent",
+                        value:"S"
+                     }
+                  ]
+               },
+               {
+                  key:"residentIndicatorStatus",
+                  label:"Resident Indicator Status",
+                  type:"select",
+                  options:[
+                     {
+                        key:"1",
+                        label:"Borrower Resident",
+                        value:"1"
+                     },
+                     {
+                        key:"2",
+                        label:"Borrower Non Resident",
+                        value:"2"
+                     }
+                  ]
+               }
+            ]
+         }
           onSubmit = {(model) => {this.onSubmit(model)}} 
         />
 
